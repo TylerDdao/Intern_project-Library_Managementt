23d67b9c36bb000553ec1838e597ef096d7b544b
Tyler Dao <baonamdao05@gmail.com>
2026-05-25 21:10:36 +0700

    Initial commit

---
+++ src/main/java/com/example/library_management/LibraryManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+		System.out.println("Hello, World");
+	}
+
+}
