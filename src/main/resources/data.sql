fdd6ab152783f811e4b98a276fdd2e64ae04eb97
TylerDdao <baonamdao05@gmail.com>
2026-06-01 09:23:08 +0700

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,18 @@
-INSERT IGNORE INTO roles (created_at, created_by, is_active, is_deleted, updated_at, updated_by, name) VALUES
-   (now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_USER'),
-   (now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_MANAGER'),
-   (now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_ADMIN');
+INSERT IGNORE INTO roles (id, created_at, created_by, is_active, is_deleted, updated_at, updated_by, name) VALUES
+   (1, now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_USER'),
+   (2, now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_MANAGER'),
+   (3, now(), 'SYSTEM', true, false, now(), 'SYSTEM', 'ROLE_ADMIN');
 
-INSERT IGNORE INTO features (feature) VALUES
-    ('CREATE_BOOK'),
-    ('CREATE_USER'),
-    ('CREAT_BORROW'),
-    ('CREATE_ROLE'),
-    ('UPDATE_BOOK'),
-    ('UPDATE_USER'),
-    ('UPDATE_BORROW'),
-    ('UPDATE_ROLE'),
-    ('DELETE_BOOK'),
-    ('DELETE_USER'),
-    ('DELETE_BORROW'),
-    ('DELETE_ROLE');
-
-
+INSERT IGNORE INTO features (id, name) VALUES
+    (1, 'CREATE_BOOK'),
+    (2, 'CREATE_USER'),
+    (3, 'CREAT_BORROW'),
+    (4, 'CREATE_ROLE'),
+    (5, 'UPDATE_BOOK'),
+    (6, 'UPDATE_USER'),
+    (7, 'UPDATE_BORROW'),
+    (8, 'UPDATE_ROLE'),
+    (9, 'DELETE_BOOK'),
+    (10, 'DELETE_USER'),
+    (11, 'DELETE_BORROW'),
+    (12, 'DELETE_ROLE');
